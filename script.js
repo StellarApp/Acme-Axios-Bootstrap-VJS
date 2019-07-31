2681a02964c3d4cfca5a1b2d8a4e37af0ffe35eb
Stella Kim <testcodesk@gmail.com>
2019-07-30 23:36:43 -0400

    completed

--- script.js
+++ script.js
@@ -1,10 +1,52 @@
-const URL="https://acme-users-api-rev.herokuapp.com/api/companies";
+const URL = "https://acme-users-api-rev.herokuapp.com/api/"
+const head = document.querySelector('thead');
+const title = document.querySelector('#title');
+const tbody = document.querySelector('tbody');
+const companies = document.querySelector('#companies') ;
+const products = document.querySelector('#products') ;
+const tabs = document.querySelectorAll('.nav-link');
 
 
-axios.get(URL)
-    .then(response => response.json())
-        .then(data => {
-            data.forEach((user)=> {
-                
-            })
-        };
+
+const loadData = (URL) => {
+    axios.get(URL)
+    .then(response => {
+        //adding table header
+        const titles = Object.keys(response.data[0]);
+        let titleHtml = '';
+        titles.forEach( title => {
+            titleHtml += `<th scope='col'>${title[0].toUpperCase()}${title.slice(1)}</th>`
+        });
+        head.innerHTML = titleHtml;
+
+        //adding table body
+        let bodyHtml = '';
+        
+        response.data.map(ele => {
+            let rowHtml = '<tr>'
+            titles.map(title => {
+                rowHtml += `<td>${ele[title]}</td>`
+            });
+            bodyHtml += `${rowHtml}</tr>`
+
+        });
+
+        tbody.innerHTML = bodyHtml;
+
+    })
+}
+
+//set default page
+window.addEventListener('load', loadData(`${URL}companies`))
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+
+    tabs.forEach((nav) => {
+        nav.classList.remove('active')
+    })
+        
+    loadData(`${URL}${window.location.hash.slice(1)}`);
+    tab.classList.toggle('active');
+    });
+})
